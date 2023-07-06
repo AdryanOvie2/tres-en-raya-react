6c118987ee8ef025a64d290fa3a22946393b2b3b
AdryanOvie2 <adryanovie2@gmail.com>
2023-07-07 00:41:49 +0200

    add useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// eslint-disable-next-line react/prop-types
+function Square({value}){
 
+  const [valor, setValor] = useState(null);
+
+  function handleClick(){
+    console.log('Clicked');
+  }
+
+  return(
+    <button 
+    className='square'
+    onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+function Board() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </>
   )
 }
 
-export default App
+export default Board
